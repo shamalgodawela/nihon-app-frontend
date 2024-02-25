b96bf62be036571ba2a8f813d1e353d7c8958c3d
shamalgodawela <112540049+shamalgodawela@users.noreply.github.com>
2024-02-25 20:32:51 +0530

    Update ProductdateList.js

--- src/compenents/product/productList/ProductdateList.js
+++ src/compenents/product/productList/ProductdateList.js
@@ -8,7 +8,7 @@ const ProductdateList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/dateProducts');
+        const response = await axios.get('https://nihon-inventory.onrender.com/api/dateProducts');
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
