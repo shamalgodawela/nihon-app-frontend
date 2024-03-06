c042ba4f2d88be83f708265b5f1fc790aff683a7
shamal melantha <shamalmelantha3@gmail.com>
2024-03-06 09:32:04 +0530

    update alloutstanding

--- src/pages/outstandingPage/AllOutStanding.js
+++ src/pages/outstandingPage/AllOutStanding.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import NavBar from '../../compenents/sidebar/NavBar'
 import Footer from '../../compenents/footer/Footer'
 import OutStandingTable from '../../compenents/outstandingTable/OutStandingTable'
+import Menu from '../../compenents/Menu/Menu'
 
 const AllOutStanding = () => {
   return (
     <div>
-        <NavBar/>
+      <Menu/>
+        
         <br/>
 
         <OutStandingTable/>
