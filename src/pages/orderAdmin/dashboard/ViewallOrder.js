0a10b26fbb5d728eb7d2fcc96d4538397b58d4af
shamal melantha <shamalmelantha3@gmail.com>
2024-03-07 10:39:20 +0530

    update

--- src/pages/orderAdmin/dashboard/ViewallOrder.js
+++ src/pages/orderAdmin/dashboard/ViewallOrder.js
@@ -22,24 +22,6 @@ const ViewallOrder = () => {
     }
   };
 
-  // Function to check if an order exists in the invoice database
-  const checkInvoice = async (orderNumber) => {
-    try {
-      const response = await fetch(`https://nihon-inventory.onrender.com/checkInvoice/${orderNumber}`);
-      const data = await response.json();
-      return data.exists; // Assuming the response contains a field indicating whether the order exists in the invoice database
-    } catch (error) {
-      console.error('Error checking invoice:', error);
-      return false;
-    }
-  };
-
-  // Function to render the checkbox based on the result of checkInvoice function
-  const renderInvoiceCheckbox = async (orderNumber) => {
-    const invoiceExists = await checkInvoice(orderNumber);
-    return <input type="checkbox" checked={invoiceExists} disabled />;
-  };
-
   return (
     <div>
       <Menu/>
@@ -53,8 +35,7 @@ const ViewallOrder = () => {
             <th className='thorder'>Invoice Number</th>
             <th className='thorder'>Order Date</th>
             <th className='thorder'>Status</th>
-            <th className='thorder'>View</th> {/* Changed from 'Action' to 'View' */}
-            <th className='thorder'>Invoice</th> {/* Added 'Invoice' column */}
+            <th className='thorder'>Action</th>
             {/* Add more table headers as needed */}
           </tr>
         </thead>
@@ -69,12 +50,10 @@ const ViewallOrder = () => {
               <td className='tdorder'>{order.orderDate}</td>
               <td className='tdorder'>{order.status}</td>
               <td className='tdorder'>
-                <Link to={`/adminorder/${order.orderNumber}`}>
-                  <AiOutlineEye size={20} color={"purple"} />
-                </Link>
-              </td>
-              <td className='tdorder'>
-                {renderInvoiceCheckbox(order.orderNumber)}
+              <Link to={`/adminorder/${order.orderNumber}`}>
+  <AiOutlineEye size={20} color={"purple"} />
+</Link>
+
               </td>
               {/* Add more table cells as needed */}
             </tr>
@@ -87,4 +66,3 @@ const ViewallOrder = () => {
 };
 
 export default ViewallOrder;
-
