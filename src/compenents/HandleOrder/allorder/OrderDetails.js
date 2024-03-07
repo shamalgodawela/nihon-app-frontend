c2b5e02239f0ebd2f15a0c40440cbfeda2e9ac72
shamal melantha <shamalmelantha3@gmail.com>
2024-03-07 09:11:19 +0530

    update orderCss

--- src/compenents/HandleOrder/allorder/OrderDetails.js
+++ src/compenents/HandleOrder/allorder/OrderDetails.js
@@ -24,30 +24,30 @@ const OrderDetails = () => {
   return (
     <div>
       <h3 className="h3order">All Order Details</h3>
-      <table>
+      <table className='tordert'>
         <thead>
           <tr>
-            <th>Order Number</th>
-            <th>Customer</th>
-            <th>Customer Code</th>
-            <th>Invoice Number</th>
-            <th>Order Date</th>
-            <th>Status</th>
-            <th>Action</th>
+            <th className='thorder'>Order Number</th>
+            <th className='thorder'>Customer</th>
+            <th className='thorder'>Customer Code</th>
+            <th className='thorder'>Invoice Number</th>
+            <th className='thorder'>Order Date</th>
+            <th className='thorder'>Status</th>
+            <th className='thorder'>Action</th>
             {/* Add more table headers as needed */}
           </tr>
         </thead>
         <tbody>
           {orders.map((order) => (
             <tr key={order._id}>
-              <td>{order.orderNumber}</td>
-              <td>{order.customer}</td>
-              <td>{order.code}</td>
-              <td>{order.invoiceNumber}</td>
+              <td className='tdorder'>{order.orderNumber}</td>
+              <td className='tdorder'>{order.customer}</td>
+              <td className='tdorder'>{order.code}</td>
+              <td className='tdorder'>{order.invoiceNumber}</td>
               {/* Add more table cells for other order details */}
-              <td>{order.orderDate}</td>
-              <td>{order.status}</td>
-              <td>
+              <td className='tdorder'>{order.orderDate}</td>
+              <td className='tdorder'>{order.status}</td>
+              <td className='tdorder'>
               <Link to={`/orders/${order.orderNumber}`}>
   <AiOutlineEye size={20} color={"purple"} />
 </Link>
