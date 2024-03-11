2536acf92710472df31edd5599ce4e116c64509b
shamal melantha <shamalmelantha3@gmail.com>
2024-03-11 09:50:52 +0530

    update orderdetails

--- src/compenents/HandleOrder/allorder/OrderDetails.js
+++ src/compenents/HandleOrder/allorder/OrderDetails.js
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './orderdetails.css';
 import { AiOutlineEye } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import { SpinnerImg } from '../../loader/Loader'; // Import loading spinner
 import { useSelector } from 'react-redux';
-import { selectIsLoading } from '../../../redux/features/product/productSlice';
-import Loader from '../../loader/Loader';
-
 
 const OrderDetails = () => {
   const [orders, setOrders] = useState([]);
-  const isLoading = useSelector(selectIsLoading);
+  const [isLoading, setIsLoading] = useState(true); // State for loading indicator
 
   useEffect(() => {
     // Fetch order details from backend API
@@ -18,53 +16,58 @@ const OrderDetails = () => {
 
   const fetchOrders = async () => {
     try {
+      setIsLoading(true); // Set loading to true when fetching starts
       const response = await fetch('https://nihon-inventory.onrender.com/api/allorders'); // Update the URL with your backend endpoint
       const data = await response.json();
       setOrders(data);
+      setIsLoading(false); // Set loading to false when fetching completes
     } catch (error) {
       console.error('Error fetching orders:', error);
+      setIsLoading(false); // Set loading to false in case of error
     }
   };
 
   return (
     <div>
-      {isLoading && <Loader/>}
-      
       <h3 className="h3order">All Order Details</h3>
-      <table className='tordert'>
-        <thead>
-          <tr>
-            <th className='thorder'>Order Number</th>
-            <th className='thorder'>Customer</th>
-            <th className='thorder'>Customer Code</th>
-            <th className='thorder'>Order Date</th>
-            <th className='thorder'>Status</th>
-            <th className='thorder'>Action</th>
-            {/* Add more table headers as needed */}
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order) => (
-            <tr key={order._id}>
-              <td className='tdorder'>{order.orderNumber}</td>
-              <td className='tdorder'>{order.customer}</td>
-              <td className='tdorder'>{order.code}</td>
-              {/* Add more table cells for other order details */}
-              <td className='tdorder'>{order.orderDate}</td>
-              <td className='tdorder'>{order.status}</td>
-              <td className='tdorder'>
-              <Link to={`/orders/${order.orderNumber}`}>
-  <AiOutlineEye size={20} color={"purple"} />
-</Link>
-
-              </td>
-              {/* Add more table cells as needed */}
+      {isLoading ? ( // Show loading spinner if isLoading is true
+        <SpinnerImg />
+      ) : (
+        <table className='tordert'>
+          <thead>
+            <tr>
+              <th className='thorder'>Order Number</th>
+              <th className='thorder'>Customer</th>
+              <th className='thorder'>Customer Code</th>
+              <th className='thorder'>Order Date</th>
+              <th className='thorder'>Status</th>
+              <th className='thorder'>Action</th>
+              {/* Add more table headers as needed */}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td className='tdorder'>{order.orderNumber}</td>
+                <td className='tdorder'>{order.customer}</td>
+                <td className='tdorder'>{order.code}</td>
+                {/* Add more table cells for other order details */}
+                <td className='tdorder'>{order.orderDate}</td>
+                <td className='tdorder'>{order.status}</td>
+                <td className='tdorder'>
+                  <Link to={`/orders/${order.orderNumber}`}>
+                    <AiOutlineEye size={20} color={"purple"} />
+                  </Link>
+                </td>
+                {/* Add more table cells as needed */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
 
 export default OrderDetails;
+
