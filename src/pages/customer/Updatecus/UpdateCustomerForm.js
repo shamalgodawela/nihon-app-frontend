1e1e5340ba894d6cef400fde3558dc55b4e42e70
shamal melantha <shamalmelantha3@gmail.com>
2024-03-12 15:41:47 +0530

    update editcus

--- src/pages/customer/Updatecus/UpdateCustomerForm.js
+++ src/pages/customer/Updatecus/UpdateCustomerForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const UpdateCustomerForm = ({ customerId }) => {
   const { customerId } = useParams();
