93903a1880d13e767c4cd988cfbfa8f0f7e53a2c
shamal melantha <shamalmelantha3@gmail.com>
2024-03-13 13:30:35 +0530

    update allproduct styles

--- src/compenents/bulkproduct/Allproduct/AllProducts.js
+++ src/compenents/bulkproduct/Allproduct/AllProducts.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './AllProducts.css';
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
@@ -22,33 +23,33 @@ const AllProducts = () => {
 
   return (
     <div>
-      <h2>All Products</h2>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Bulk Code</th>
-              <th>Quantity</th>
-              <th>Weight</th>
-              <th>Total Weight</th>
+    <h2>All Products</h2>
+    {isLoading ? (
+      <p>Loading...</p>
+    ) : (
+      <table className="product-table"> {/* Apply CSS class to the table */}
+        <thead>
+          <tr>
+            <th>Bulk Code</th>
+            <th>Quantity</th>
+            <th>Weight</th>
+            <th>Total Weight</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(product => (
+            <tr key={product._id}>
+              <td>{product.bulkCode}</td>
+              <td>{product.quantity}</td>
+              <td>{product.weight}</td>
+              <td>{product.totweight}</td>
+              {/* Add more table cells if needed */}
             </tr>
-          </thead>
-          <tbody>
-            {products.map(product => (
-              <tr key={product._id}>
-                <td>{product.bulkCode}</td>
-                <td>{product.quantity}</td>
-                <td>{product.weight}</td>
-                <td>{product.totweight}</td>
-                {/* Add more table cells if needed */}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+          ))}
+        </tbody>
+      </table>
+    )}
+  </div>
   );
 };
 
