d5290cb5b4d07e1dc468e014a5787eb36c234697
shamal melantha <shamalmelantha3@gmail.com>
2024-05-16 14:37:22 +0530

    update menu.js

--- src/compenents/Menu/Menu.js
+++ src/compenents/Menu/Menu.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './menu.css'
 import { LuRefreshCw } from 'react-icons/lu';
+import { IoMdArrowRoundBack } from 'react-icons/io';
 
 const Menu = () => {
+  const navigate=useNavigate();
+
   const RefreshButton = () => {
     const handleRefresh = () => {
       window.location.reload();
     };
+    
 
     return (
       <button onClick={handleRefresh} className="refresh-button"><LuRefreshCw /></button>
     );
   };
+  const goBack = () => {
+    // Use navigate(-1) to navigate back
+    navigate(-1);
+   };
   return (
     <div>
     <nav className="navbar2">
@@ -31,6 +39,7 @@ const Menu = () => {
     
     
   </nav>
+  <Link to="#" onClick={goBack}><IoMdArrowRoundBack size={23}/></Link>&nbsp;&nbsp;
   <RefreshButton />
   </div>
   );
