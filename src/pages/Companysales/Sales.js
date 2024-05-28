5fa55ba1d7a6194c1005f6188e05c2189e07f196
shamal melantha <shamalmelantha3@gmail.com>
2024-05-28 11:23:25 +0530

    update sales

--- src/pages/Companysales/Sales.js
+++ src/pages/Companysales/Sales.js
@@ -6,6 +6,8 @@ import './sales.css';
 
 const Sales = () => {
   const [totalSales, setTotalSales] = useState(0);
+  const [totalcollection, settotalcollection]=useState(0);
+  const [outstanding, setoutstanding]= useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -22,8 +24,35 @@ const Sales = () => {
       }
     };
 
+    const fetchTotalcollection= async ()=>{
+      try{
+        const response= await axios.get(`https://nihon-inventory.onrender.com/api/sumofcollection`);
+        settotalcollection(response.data.sum);
+        setLoading(false);
+
+      }
+      catch(error){
+        console.error('failed to fetch total collection',error);
+        setError('failed to fetch total collection')
+        setLoading(false);
+
+      }
+
+    };
+
     fetchTotalSales();
+    fetchTotalcollection();
   }, []);
+
+  useEffect(()=>{
+    setoutstanding(totalSales-totalcollection);
+  },[totalSales,totalcollection]);
+
+  
+
+  const formatNumbers = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
 
   return (
     <div>
@@ -31,21 +60,55 @@ const Sales = () => {
       <div className='sales-Heading'>
         <h3>Hi, Welcome back!</h3>
         <h4>Finance Performance and Monitoring Sales Performance</h4>
+        <h4 className='h4-season-update-heading'>Updated details of 2024 april to present(yala season)</h4>
       </div>
-      <div className="card">
-        <div className="card-header">
-          <h2>Total Sales</h2>
+      <div className="cards-container">
+        <div className="card">
+          <div className="card-header">
+            <h2>Total Sales</h2>
+          </div>
+          <div className="card-body">
+            {loading ? (
+              <p>Loading...</p>
+            ) : error ? (
+              <p>{error}</p>
+            ) : (
+              <p>RS/= {formatNumbers(totalSales.toFixed(2))}</p>
+            )}
+          </div>
         </div>
-        <div className="card-body">
-          {loading ? (
-            <p>Loading...</p>
-          ) : error ? (
-            <p>{error}</p>
-          ) : (
-            <p>RS/= {totalSales.toLocaleString()}</p>
-          )}
+
+        <div className="card">
+          <div className="card-header">
+            <h2>Total Collection</h2>
+          </div>
+          <div className="card-body">
+            {loading ? (
+              <p>Loading...</p>
+            ) : error ? (
+              <p>{error}</p>
+            ) : (
+              <p>RS/= {formatNumbers((totalcollection.toFixed(2)))}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-header">
+            <h2>Total Outstanding</h2>
+          </div>
+          <div className="card-body">
+            {loading ? (
+              <p>Loading...</p>
+            ) : error ? (
+              <p>{error}</p>
+            ) : (
+              <p>RS/= {formatNumbers(outstanding.toFixed(2))}</p>
+            )}
+          </div>
         </div>
       </div>
+     
       <Footer />
     </div>
   );
