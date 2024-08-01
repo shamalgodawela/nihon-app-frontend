816288fc23a2225d4259c0054c398118deb9bd67
shamal melantha <shamalmelantha3@gmail.com>
2024-08-01 13:03:48 +0530

    update dealer history page

--- src/pages/orderAdmin/CheckdealerHistory/DealerHistory.js
+++ src/pages/orderAdmin/CheckdealerHistory/DealerHistory.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Menu from '../../../compenents/Menu/Menu'
+import './delaer.css'
 
 const DealerHistory = () => {
   return (
     <div>
         <Menu/>
         <div>
-            <h1>Under Construction</h1>
+            <h1 className='h1-dealer-history'>Under Construction</h1>
         </div>
     </div>
   )
