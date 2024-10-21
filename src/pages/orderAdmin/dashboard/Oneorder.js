c7c021f9681d5dae1fc96770b1527bce6494b8ee
shamal melantha <shamalmelantha3@gmail.com>
2024-10-21 12:46:16 +0530

    update frontend

--- src/pages/orderAdmin/dashboard/Oneorder.js
+++ src/pages/orderAdmin/dashboard/Oneorder.js
@@ -227,7 +227,7 @@ const Oneorder = () => {
                     <option value="">Select Approved or Cancel</option>
                     <option value="Approved">Approved</option>
                     <option value="Canceled">canceled</option>
-                    <option value="Canceled">Pending</option>
+                    <option value="Pending">Pending</option>
                   </select>
                 </div>
                 <h3 className="order-details-product-header">Products</h3>
