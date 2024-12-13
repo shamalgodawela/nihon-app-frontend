fd8723cd6fe61cab0cddf7fbcd854b25e216791a
shamal melantha <shamalmelantha3@gmail.com>
2024-12-13 15:02:54 +0530

    up

--- src/compenents/Exedashboard/Exedashboard.js
+++ src/compenents/Exedashboard/Exedashboard.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import HeaderExe from '../headerexe/HeaderExe';
+import HeaderE from '../header/HeaderE';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../../redux/features/product/productSlicaExe';
-import { selectIsLoggedIn, selectName, selectInvoices } from '../../redux/features/auth/authSliceExe';
+import { selectIsLoggedIn, selectName } from '../../redux/features/auth/authSliceExe';
 import ProductListExe from '../product/productList/ProductListExe';
+import axios from 'axios';
 
 const Exedashboard = () => {
   const name = useSelector(selectName);
   const dispatch = useDispatch();
   const isLoggedin = useSelector(selectIsLoggedIn);
-  const invoices = useSelector(selectInvoices); // Access invoices from Redux state
   const { products, isLoading, isError, message } = useSelector((state) => state.product);
+
+  const [invoices, setInvoices] = useState([]);
+  const [invoiceLoading, setInvoiceLoading] = useState(true);
 
   useEffect(() => {
     if (isLoggedin) {
       dispatch(getProducts());
+      fetchInvoices();
     }
 
     if (isError) {
@@ -21,13 +27,32 @@ const Exedashboard = () => {
     }
   }, [isLoggedin, isError, message, dispatch]);
 
+  const fetchInvoices = async () => {
+    try {
+      const response = await axios.get('https://nihon-inventory.onrender.com/api/get-all-invoices', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`, 
+        },
+      });
+      setInvoices(response.data.invoices || []);
+      setInvoiceLoading(false);
+    } catch (error) {
+      console.error('Error fetching invoices:', error);
+      setInvoiceLoading(false);
+    }
+  };
+
   return (
     <div>
+      <HeaderExe />
+      <HeaderE />
       <h2>Welcome, {name}</h2>
       <ProductListExe products={products} />
       <div>
         <h3>Invoices</h3>
-        {invoices.length > 0 ? (
+        {invoiceLoading ? (
+          <p>Loading invoices...</p>
+        ) : invoices.length > 0 ? (
           <table className="table table-bordered">
             <thead>
               <tr>
@@ -36,7 +61,7 @@ const Exedashboard = () => {
                 <th>Code</th>
                 <th>Address</th>
                 <th>Contact</th>
-            
+                <th>Invoice Date</th>
               </tr>
             </thead>
             <tbody>
@@ -47,7 +72,7 @@ const Exedashboard = () => {
                   <td>{invoice.code}</td>
                   <td>{invoice.address}</td>
                   <td>{invoice.contact}</td>
-
+                  <td>{new Date(invoice.invoiceDate).toLocaleDateString()}</td>
                 </tr>
               ))}
             </tbody>
